2add135e1484e6138e31ebbb0c8d4cef70ba2d40
Jason Suttles <jasonssuttles@gmail.com>
2017-04-23 16:31:49 -0400

    _hasPrivilegeLevel check

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,7 +5,7 @@ import constants from '../utils/constants';
 
 const router = express.Router();
 
-router.get('/registerUser', utils.hasPrivilegeLevel.bind(null, constants.PRIVILEGES.EDITOR), handlers.registerUser);
+router.get('/registerUser', utils.hasPrivilegeLevel.bind(null, constants.PRIVILEGES.EDITOR.ENUM), handlers.registerUser);
 router.get('/login', handlers.login);
 router.get('/logout', utils.isLoggedIn, handlers.logout);
 router.get('/auth', handlers.auth);
