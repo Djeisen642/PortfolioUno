bf9d21b1df85f48ca6899cc8d24ae4d8a5d36aab
Jason Suttles <jasonssuttles@gmail.com>
2017-04-23 23:56:00 -0400

    missed this merge conflict

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,16 +5,13 @@ import constants from '../utils/constants';
 
 const router = express.Router();
 
-<<<<<<< HEAD
 router.get(
   '/registerUser',
   utils.isLoggedIn,
-  utils.hasPrivilegeLevel.bind(null, constants.PRIVILEGES.EDITOR),
+  utils.hasPrivilegeLevel.bind(null, constants.PRIVILEGES.EDITOR.ENUM),
   handlers.registerUser
 );
-=======
-router.get('/registerUser', utils.hasPrivilegeLevel.bind(null, constants.PRIVILEGES.EDITOR.ENUM), handlers.registerUser);
->>>>>>> 2add135e1484e6138e31ebbb0c8d4cef70ba2d40
+
 router.get('/login', handlers.login);
 router.get('/logout', utils.isLoggedIn, handlers.logout);
 router.get('/auth', handlers.auth);
